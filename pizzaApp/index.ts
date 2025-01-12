aa5f66ffc3721768e461ca4d9840b4b87581334d
benshabbat <benshabbat27@gmail.com>
2025-01-12 11:16:53 +0200

    placeOrder

--- pizzaApp/index.ts
+++ pizzaApp/index.ts
@@ -1,8 +1,8 @@
 const menu [
     {name:"margarita", price: 5},
     {name:"Olives", price: 7},
-    {name:"vegetarian", price: 6}
-    {name:"mushrooms", price: 8}
+    {name:"vegetarian", price: 6},
+    {name:"mushrooms", price: 8},
 ]
 
 
@@ -13,3 +13,13 @@ function addNewPizza(pizzaObj){
     menu.push(pizzaObj)
 }
 
+function placeOrder(namePizza){
+    const pizza = menu.find(pizza => pizza.name === namePizza)
+    cashInRegister += pizza.price;
+    const newOrder ={
+        pizzaName: pizza.name,
+        status:"orderded"
+    }
+    orderQue.push(newOrder)
+    return newOrder
+}
