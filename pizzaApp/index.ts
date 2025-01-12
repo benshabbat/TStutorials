dc560feb0d9d6c0df7c53eea48fcc640365a30ef
benshabbat <benshabbat27@gmail.com>
2025-01-12 10:59:03 +0200

    addNewPizza

---
+++ pizzaApp/index.ts
@@ -0,0 +1,15 @@
+const menu [
+    {name:"margarita", price: 5},
+    {name:"Olives", price: 7},
+    {name:"vegetarian", price: 6}
+    {name:"mushrooms", price: 8}
+]
+
+
+const cashInRegister = 100;
+const orderQue=[]
+
+function addNewPizza(pizzaObj){
+    menu.push(pizzaObj)
+}
+
